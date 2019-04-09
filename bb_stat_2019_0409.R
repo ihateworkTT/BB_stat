8ac6e6ec67b26bae0f596fbe0d211c3a7596e943
ihateworkTT <e10n910@gmail.com>
2019-04-09 17:57:39 +0900

    BB_stat

--- bb_stat_2019_0409.R
+++ bb_stat_2019_0409.R
@@ -1,19 +1,17 @@
 #file importing
 library(RCurl)
 x<-getURL("https://raw.githubusercontent.com/ihateworkTT/BB_stat/master/bb_ind_hitter_stat.csv")
-head(x)
 bb_stat <- read.csv(text=x)
 names(bb_stat)[1] <- c("Year")
 head(bb_stat)
 
-#calculate basic stat (AVG, OBP, SLG, OPS)
-bb_stat <- transform(bb_stat, AVG = round(H/AB,3), OBP=round((H+BB+HBP)/(AB+BB+HBP+SH+SF),3), SLG=round(((H-B2-B3-HR)+B2*2+B3*3+HR*4)/AB,3))
-bb_stat <- transform(bb_stat, OPS = OBP + SLG)
-
 #AVG < 0.3
 bb_stat1 <- bb_stat[bb_stat$AVG<0.3,]
-bb_stat1
 
+#regual AB
+nrow(bb_stat)
+#the numbe of the hitter, Average < 0.3
+nrow(bb_stat1)
 
 transform(bb_stat, PorC = ifelse(OBP>SLG,1,-1))
 
